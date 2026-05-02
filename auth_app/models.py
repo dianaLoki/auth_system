a31acfa6a1f6987b5782cc52a944ac32a0a19b58
Diana Volkova <Volkova6575@gmail.com>
2026-05-02 19:39:20 +0300

    feat: add RefreshToken model

--- auth_app/models.py
+++ auth_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+class RefreshToken(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=512, unique=True)
+    expires_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
