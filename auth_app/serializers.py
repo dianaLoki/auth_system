625eae2ae5f4976fbba6aefff16ea093e66e909f
Diana Volkova <Volkova6575@gmail.com>
2026-05-02 20:22:19 +0300

    add register endpoint

---
+++ auth_app/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from users.models import User
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    password_confirm = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['email', 'first_name', 'last_name', 'patronymic', 'password', 'password_confirm']
+
+    def validate(self, data):
+        if data['password'] != data['password_confirm']:
+            raise serializers.ValidationError('Пароли не совпадают')
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop('password_confirm')
+        return User.objects.create_user(**validated_data)
