cf2e526ed6f6bb58a837a3d787face2f42f6935e
Diana Volkova <Volkova6575@gmail.com>
2026-05-02 20:04:20 +0300

    connect JWT authentication to DRF settings

--- auth_project/settings.py
+++ auth_project/settings.py
@@ -135,3 +135,12 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'utils.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
