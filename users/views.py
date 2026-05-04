09bf759e9672909c2d977fed694dd8128f406ef6
Diana Volkova <Volkova6575@gmail.com>
2026-05-04 22:40:22 +0300

    Add user profile endpoints: get, update, soft delete

--- users/views.py
+++ users/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from users.serializers import UserSerializer
 
-# Create your views here.
+
+class UserMeView(APIView):
+
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def patch(self, request):
+        serializer = UserSerializer(request.user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request):
+        request.user.is_active = False
+        request.user.save()
+        return Response(
+            {'message': 'Аккаунт успешно удалён'},
+            status=status.HTTP_200_OK
+        )
