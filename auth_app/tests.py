2b857d8d3f4b3f7e3d1af8ef3bc0f56e44f93423
Diana Volkova <Volkova6575@gmail.com>
2026-05-05 13:05:00 +0300

    Add login tests for auth_app

--- auth_app/tests.py
+++ auth_app/tests.py
@@ -1,3 +1,57 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from users.models import User
+from access.models import Role, RolePermission, UserRole
+from django.urls import reverse
 
-# Create your tests here.
+class ViewAuthAppTest(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            email='test_email@gmail.com',
+            password='12345',
+            first_name='test',
+            last_name='test',
+            patronymic='test'
+        )
+
+    def test_register_view(self):
+        url = reverse('register')
+        response = self.client.post(url, data={
+            'email': 'new_user@gmail.com',
+            'password': '12345',
+            'password_confirm': '12345',
+            'first_name': 'test',
+            'last_name': 'test',
+            'patronymic': 'test'
+        })
+        self.assertEqual(response.status_code, 201)
+
+    def test_register_400(self):
+        url = reverse('register')
+        response = self.client.post(url, data={
+            'email': 'test_email@gmail.com',
+            'password': '12345',
+            'password_confirm': '12345',
+            'first_name': 'test',
+            'last_name': 'test',
+            'patronymic': 'test'
+        })
+        self.assertEqual(response.status_code, 400)
+
+
+    def test_login_view(self):
+        url = reverse('login')
+        response = self.client.post(url, data={
+            'email': 'test_email@gmail.com',
+            'password': '12345',
+        })
+        self.assertIn('access_token', response.data)
+        self.assertIn('refresh_token', response.data)
+
+    def test_login_view_401(self):
+        url = reverse('login')
+        response = self.client.post(url, data={
+            'email': 'test_email@gmail.com',
+            'password': '111111',
+        })
+        self.assertEqual(response.status_code, 401)
